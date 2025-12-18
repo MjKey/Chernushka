378df581d6257b7e8d532b8d1d51b3adec1a4b26
MjKey <go@mjkey.ru>
2025-12-18 13:24:41 +0300

    Добавлено яйцо спавна чернушки
    Теперь можно призывать чернушек из креативки! Яйцо появится во вкладке Spawn Eggs. Добавлены переводы на русский и английский.

--- src/main/java/ru/MjKey/chernushka/item/ModItems.java
+++ src/main/java/ru/MjKey/chernushka/item/ModItems.java
@@ -3,12 +3,14 @@ package ru.MjKey.chernushka.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import ru.MjKey.chernushka.Chernushka;
+import ru.MjKey.chernushka.entity.ModEntities;
 
 public class ModItems {
     
@@ -16,6 +18,13 @@ public class ModItems {
         new ChernushkaStickItem(new Item.Settings()
             .maxCount(1)
             .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Chernushka.MOD_ID, "chernushka_stick")))
+        )
+    );
+    
+    public static final Item CHERNUSHKA_SPAWN_EGG = registerItem("ch_egg",
+        new SpawnEggItem(ModEntities.CHERNUSHKA, 0x1a1a1a, 0x4a4a4a,
+            new Item.Settings()
+                .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Chernushka.MOD_ID, "ch_egg")))
         )
     );
     
@@ -29,6 +38,11 @@ public class ModItems {
         // Добавляем в креативную вкладку инструментов
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
             entries.add(CHERNUSHKA_STICK);
+        });
+        
+        // Добавляем яйцо спавна в креативную вкладку
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> {
+            entries.add(CHERNUSHKA_SPAWN_EGG);
         });
     }
 }
